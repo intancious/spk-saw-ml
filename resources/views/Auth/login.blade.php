22125ae6ab2217853b10c17a08317307cd597aa1
intancious <intancious98@gmail.com>
2025-10-08 01:16:42 +0700

    gass pokok bisa running

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,39 +1,96 @@
-@extends('Auth.layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', 'Login')
+<head>
 
-@section('content')
-    <div class="container">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login - SPK</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('template/backend') }}/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container mt-5">
+
+        <!-- Outer Row -->
         <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card shadow-lg">
-                    <div class="card-body">
-                        <h3 class="text-center mb-4 text-gray-900">Login Account</h3>
 
-                        @if ($errors->any())
-                            <div class="alert alert-danger text-center">
-                                @foreach ($errors->all() as $error)
-                                    <div>{{ $error }}</div>
-                                @endforeach
-                            </div>
-                        @endif
+            <div class="col-xl-5 col-lg-12 col-md-9">
 
-                        <form method="POST" action="{{ url('/login') }}">
-                            @csrf
-                            <div class="form-group mb-3">
-                                <input type="text" name="username" value="{{ old('username') }}" class="form-control"
-                                    placeholder="Username" required autofocus>
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            {{-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> --}}
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+
+                                    @if ($errors->any())
+                                        <div class="alert alert-danger">
+                                            <ul>
+                                                @foreach ($errors->all() as $error)
+                                                    <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    @endif
+                                    @include('sweetalert::alert')
+
+                                    <form class="user" method="POST" action="{{ route('login.proses') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input type="text" name="username" value="{{ old('username') }}"
+                                                class="form-control form-control-user"
+                                                placeholder="Masukkan Username Anda">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="password"
+                                                class="form-control form-control-user" id="exampleInputPassword"
+                                                placeholder="Password">
+                                        </div>
+
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                                    </form>
+                                </div>
                             </div>
-                            <div class="form-group mb-3">
-                                <input type="password" name="password" class="form-control" placeholder="Password" required>
-                            </div>
-                            <button type="submit" class="btn btn-danger btn-block">
-                                <i class="fas fa-sign-in-alt mr-1"></i> Masuk
-                            </button>
-                        </form>
+                        </div>
                     </div>
                 </div>
+
             </div>
+
         </div>
+
     </div>
-@endsection
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('template/backend') }}/vendor/jquery/jquery.min.js"></script>
+    <script src="{{ asset('template/backend') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('template/backend') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('template/backend') }}/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
