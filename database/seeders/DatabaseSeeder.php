8f4365da49404a74012185eb50c2dc26264813e8
intancious <intancious98@gmail.com>
2025-10-08 01:05:54 +0700

    solved conflic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,19 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         // $this->call(PositionSeeder::class);
         // $this->call(ShiftSeeder::class);
         $this->call(UserSeeder::class);
         // $this->call(LocationSeeder::class);
         // $this->call(PenaltiesSeeder::class);
-=======
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
->>>>>>> 1b2868bb53a74e3e94e1a63d99eeadbfe3a1727c
     }
 }
