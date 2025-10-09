e0cd8b95819966b4fcb7cf86a03b0f058b284d3e
ingeeef <ingefatulkomariah@gmail.com>
2025-10-10 04:18:39 +0700

    update index kriteria

--- resources/views/Admin/pages/alternatif/index.blade.php
+++ resources/views/Admin/pages/alternatif/index.blade.php
@@ -4,12 +4,11 @@
 ])
 
 @section('content')
-    <h1 class="h3 mb-2 text-gray-800">Data Alternatif</h1>
+ <h2 class="mb-2 text-gray-800"><i class="fas fa-users"></i> Data Alternatif</h2>
 
     <div class="card shadow mb-4">
-        <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
-            <h6 class="m-0 font-weight-bold">Daftar Alternatif</h6>
-            <a href="{{ route('alternatif.create') }}" class="btn btn-light btn-sm">
+        <div class="card-header py-3">
+            <a href="{{ route('alternatif.create') }}" class="btn btn-primary btn-sm">
                 <i class="fas fa-plus"></i> Tambah Alternatif
             </a>
         </div>
